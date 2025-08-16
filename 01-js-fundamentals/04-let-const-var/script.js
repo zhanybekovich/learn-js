d755aeda5c512a6b2ead3a0aae923c04f528cd4a
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-16 21:56:59 +0600

    let, const, var

---
+++ 01-js-fundamentals/04-let-const-var/script.js
@@ -0,0 +1,20 @@
+let age = 30;
+console.log(age);
+
+// re-assigning
+age = 35;
+console.log(age);
+
+// const variables
+const birthYear = 1991;
+console.log(birthYear);
+
+// error
+// birthYear = 2000;
+
+// var keyword -> don't use var
+var job = "Programmer";
+console.log(job);
+
+job = "Designer";
+console.log(job);
