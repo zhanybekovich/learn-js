8448059c0fc62162682a8bb23a2dda9d485c36f9
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-16 19:40:45 +0600

    variables

---
+++ 01-js-fundamentals/02-values-and-variables/script.js
@@ -0,0 +1,20 @@
+// Values
+// Value is a piece of data. The most fundamental unit of information.
+console.log("John");
+console.log(20 + 20 - 5);
+
+// Variables
+// Values can be stored in variables.
+let firstName = "John";
+console.log(firstName);
+
+// change value of the variable
+firstName = "Bob";
+console.log(firstName);
+
+// naming rules
+// - use camelCase
+// - don't start var name with number eg: let 12user
+// - var name can contain number: eg let user1
+// - variable can start with $ sign eg: let $user
+// - don't use reserved JS keywords eg: let new
