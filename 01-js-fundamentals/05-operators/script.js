188ca89bc628182b8c09aef3404566484c148922
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-16 22:22:15 +0600

    basic operators

---
+++ 01-js-fundamentals/05-operators/script.js
@@ -0,0 +1,80 @@
+// Basic operators
+
+// arithmetics operations
+
+// addition
+console.log(10 + 10);
+let a = 5;
+let b = 4;
+console.log(a + b);
+
+// subtraction
+console.log(10 - 3);
+console.log(a - b);
+
+// multiplication
+console.log(10 * 4);
+console.log(a * b);
+
+// division
+console.log(45 / 4);
+console.log(a / b);
+
+// modulus
+console.log(5 % 3);
+console.log(a % b);
+
+// exponentiation
+console.log(2 ** 6);
+console.log(a ** b);
+
+// operator precedence
+console.log(2 + 2 * 2);
+console.log((2 + 2) * 2);
+
+// concatenation
+let firsName = "John";
+let lastName = "Snider";
+let fullName = firsName + " " + lastName;
+console.log(fullName);
+
+// assignment
+let x = 10;
+x += 10;
+console.log(x);
+
+let y = 5;
+y -= 2;
+console.log(y);
+
+let f = 2;
+f *= 2;
+console.log(f);
+
+let h = 30;
+h /= 10;
+console.log(h);
+
+// increment
+let g = 1;
+// g++;
+++g;
+console.log(g);
+
+// decrement
+let i = 4;
+// i--;
+--i;
+console.log(i);
+
+// comparison
+console.log(5 > 3);
+console.log(5 < 3);
+console.log(5 == 5);
+console.log(5 == "5");
+console.log(5 === "5");
+console.log(5 >= 5);
+console.log(5 <= 5);
+console.log(5 != 4);
+console.log(5 != "5");
+console.log(5 !== "5");
