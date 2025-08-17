3cb17803a150cafbb90e8ce77867dc96a9d9e886
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-17 14:26:22 +0600

    ternary operator

---
+++ 01-js-fundamentals/15-ternary-operator/script.js
@@ -0,0 +1,9 @@
+/**
+ * Ternary Operator
+ */
+
+const age = 23;
+age >= 18 ? console.log("Can vote!") : console.log("Can not vote");
+
+const canVote = age >= 18 ? "vote" : "not vote";
+console.log(`Can ${canVote}`);
