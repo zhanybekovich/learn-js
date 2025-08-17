fc7d3f2abeb0a3a05a7dd3e27e1ae70732b77452
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-17 14:52:45 +0600

    JS releases

---
+++ 01-js-fundamentals/17-js-releases/script.js
@@ -0,0 +1,14 @@
+/**
+ * 1195 - Brendan Eich creates the very first version of JS called Mocha
+ *
+ * 1996 - Mocha changes to LiveScript and then to JavaScript to attract Java developers. Microsoft launches IE, copying JavaScript form Netscape and calling it JScript.
+ *
+ * 1997 - Need to standardize the language. ECMA releases ECMAScript 1 (ES1) the first official standard
+ *
+ * 2009 - ES5 is released
+ *
+ * 2015 - ES6 (ECMAScript 2015) was released with the BIGGEST update. ECMA changes to an annual release cycle in order to ship less features per update
+ *
+ * since 2016 - ES2016 / ES2017 / ES2018 / ES2019 / ES2020 ...
+ *
+ */
