e0d9491e1d9dec03017829289df8b3fd119935f3
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-18 19:43:55 +0600

    handling click event

--- 03-guess-my-number/script.js
+++ 03-guess-my-number/script.js
@@ -1,9 +1,9 @@
 "use strict";
 
-console.log(document.querySelector(".message").textContent);
-document.querySelector(".message").textContent = "Correct Number!";
-console.log(document.querySelector(".message").textContent);
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = document.querySelector(".guess").value;
 
-document.querySelector(".number").textContent = 15;
-document.querySelector(".score").textContent = 90;
-document.querySelector(".guess").value = 14;
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔ No number!";
+  }
+});
