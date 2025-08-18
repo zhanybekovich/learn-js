338ab88ff3042207ac7ef063e7bb5692639e4405
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-18 12:00:12 +0600

    intro to objects

---
+++ 02-js-fundamentals-2/10-objects/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/**
+ * Objects
+ */
+
+const john = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 2025 - 1999,
+  jog: "teacher",
+  friends: ["Mike", "Bob", "Peter"],
+  isMarried: false,
+};
+
+console.log(john);
