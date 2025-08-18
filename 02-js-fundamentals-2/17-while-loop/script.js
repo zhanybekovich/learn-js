fe23d971fdcdb90ff35fe0e8e626464ff029e407
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-18 14:39:27 +0600

    while, do while loop

---
+++ 02-js-fundamentals-2/17-while-loop/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/**
+ * While Loop
+ */
+let i = 0;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+console.log("==========");
+
+let j = 10;
+while (j > 0) {
+  console.log(j);
+  j--;
+}
+
+console.log("==========");
+
+// do while
+let x = 2;
+do {
+  console.log(x);
+  x--;
+} while (x > 11);
