3f9f2100d42ac3ff43859edd5982da3502517599
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-19 10:15:30 +0600

    manupilating css

--- 03-guess-my-number/script.js
+++ 03-guess-my-number/script.js
@@ -14,6 +14,10 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent = "⛔ No number!";
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Correct Number!";
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+
+    document.querySelector(".number").style.width = "30rem";
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "Too high!";
