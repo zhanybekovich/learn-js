9e119dd8d1997f73426c51afcce77cfb7513f48c
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-19 10:33:22 +0600

    fix selector class

--- 03-guess-my-number/script.js
+++ 03-guess-my-number/script.js
@@ -48,5 +48,5 @@ document.querySelector(".again").addEventListener("click", function () {
   document.querySelector(".number").textContent = "?";
   document.querySelector(".guess").value = "";
   document.querySelector("body").style.backgroundColor = "#222";
-  document.querySelector("number").style.width = "15rem";
+  document.querySelector(".number").style.width = "15rem";
 });
