819d3d13bd8c2e57e5ce18b31e5bb8b58c2b60cc
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-19 16:36:48 +0600

    switching the active player

--- 05-pig-game/script.js
+++ 05-pig-game/script.js
@@ -8,12 +8,16 @@ const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 const current0El = document.querySelector("#current--0");
 const current1El = document.querySelector("#current--1");
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 btnRoll.addEventListener("click", function () {
   const dice = Math.trunc(Math.random() * 6 + 1);
@@ -23,7 +27,13 @@ btnRoll.addEventListener("click", function () {
 
   if (dice !== 1) {
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
   }
 });
