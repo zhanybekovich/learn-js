1fe5da35e1afd270039ced40f4af3f1f5484c6fe
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-19 13:15:55 +0600

    open & show modal

--- 04-modal/script.js
+++ 04-modal/script.js
@@ -5,6 +5,19 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
+function closeModal() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 }
+
+function openModal() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModal);
+}
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
