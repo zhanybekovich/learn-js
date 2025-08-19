595e789afda4bc7ede62b7e0dc49d361a73992ee
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-19 10:40:31 +0600

    implement highscore

--- 03-guess-my-number/script.js
+++ 03-guess-my-number/script.js
@@ -2,6 +2,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector(".score").textContent = score;
 
@@ -18,6 +19,11 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector("body").style.backgroundColor = "#60b347";
 
     document.querySelector(".number").style.width = "30rem";
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "Too high!";
