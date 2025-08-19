61ecd15dacc15ddc5b3d450ca2b9a1c4686ade38
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-19 10:07:14 +0600

    implementing game logic

--- 03-guess-my-number/script.js
+++ 03-guess-my-number/script.js
@@ -1,9 +1,36 @@
 "use strict";
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector(".score").textContent = score;
+
+document.querySelector(".number").textContent = secretNumber;
+
 document.querySelector(".check").addEventListener("click", function () {
-  const guess = document.querySelector(".guess").value;
+  const guess = Number(document.querySelector(".guess").value);
 
   if (!guess) {
     document.querySelector(".message").textContent = "⛔ No number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Correct Number!";
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You Lost the Game!";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You Lost the Game!";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
