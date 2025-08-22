0ee1350288da906ab95d7642ae49caf3755818fd
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-22 16:30:52 +0600

    default func params

---
+++ 12-closer-look-at-funcs/01-default-params/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const bookings = [];
+
+// function with default params
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking("LH123");
+createBooking("BS", 2);
+createBooking("LA234", 4, 600);
