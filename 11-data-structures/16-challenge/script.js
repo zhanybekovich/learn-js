d4a25195843517cfd4bfad9258f7f2c2d3ae228f
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-22 10:47:47 +0600

    challenge

---
+++ 11-data-structures/16-challenge/script.js
@@ -0,0 +1,118 @@
+"use strict";
+
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  data: "Nov 9th, 2025",
+  odds: {
+    team1: 11.33,
+    x: 3.25,
+    team2: 5.6,
+  },
+};
+
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
+console.log(players1Final);
+
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+const printGoals = function (...players) {
+  console.log(`${players.length} goals were scored!`);
+};
+printGoals("Davies", "Muller", "Kimmich");
+printGoals("Davies", "Muller");
+printGoals(...game.scored);
+
+team1 < team2 && console.log("Team 1 is more likely to win");
+team1 > team2 && console.log("Team 2 is more likely to win");
+
+console.log("=============");
+
+for (const [i, item] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${item}`);
+}
+
+const odds = Object.values(game.odds);
+let average = 0;
+for (const odd of odds) {
+  average += odd;
+}
+average /= odds.length;
+console.log(average);
+
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === "x" ? "draw" : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr} ${odd}`);
+}
+
+console.log("============");
+
+const gameEvents = new Map([
+  [17, "Goal"],
+  [36, "Substitution"],
+  [47, "Goal"],
+  [61, "Substitution"],
+  [64, "Yellow card"],
+  [69, "Red card"],
+  [70, "Substitution"],
+  [72, "Substitution"],
+  [76, "Goal"],
+  [80, "Goal"],
+  [92, "Yellow card"],
+]);
+
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+gameEvents.delete(64);
+
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+for (const [min, event] of gameEvents) {
+  const halt = min <= 45 ? "FIRST" : "SECOND";
+  console.log(`${halt} [ HALF ] ${min}: ${event}`);
+}
