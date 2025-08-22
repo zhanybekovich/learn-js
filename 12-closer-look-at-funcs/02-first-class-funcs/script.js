a3d497656217cb42440068bc859ae296416fe7af
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-22 16:47:01 +0600

    first-class & high-order functions

---
+++ 12-closer-look-at-funcs/02-first-class-funcs/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/**
+ * first-class functions -> functions are simply values
+ * functions are just another type of object
+ */
+
+/**
+ * high-order function -> a function that receives another function as an argument, that returns a new function or both
+ */
