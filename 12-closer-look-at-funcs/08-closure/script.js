fe40c3602836a23bb13ea64f24b2d0a27a915723
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-23 10:32:53 +0600

    closure

---
+++ 12-closer-look-at-funcs/08-closure/script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const lufthansa = {
+  airline: "Lufthansa",
+  iataCode: "LH",
+  bookings: [],
+};
+
+const eurowings = {
+  airline: "Eurowings",
+  iataCode: "EW",
+  bookings: [],
+};
+
+function book(flightNum, name) {
+  console.log(
+    `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+  );
+  this.bookings.push({ flight: `${this.iataCode}${flightNum}, ${name}` });
+}
+
+/**
+ * Closure
+ */
+
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(passengerCount);
+  };
+};
+
+const booker = secureBooking();
+booker();
+booker();
+
+const booker2 = secureBooking();
+booker2();
+booker2();
+booker2();
+booker2();
+booker2();
