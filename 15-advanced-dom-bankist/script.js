de75c201b8e0d84afd781d10cd6ebb60477f5ddf
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-26 20:08:39 +0600

    passing arguments into event handler function

--- 15-advanced-dom-bankist/script.js
+++ 15-advanced-dom-bankist/script.js
@@ -81,5 +81,23 @@ tabsContainer.addEventListener('click', function (e) {
     .classList.add('operations__content--active');
 });
 
+// Menu fade animation
+function handleHover(e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+      logo.style.opacity = this;
+    });
+  }
+}
+
+const nav = document.querySelector('.nav');
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
 ///////////////////////////////
 ///////////////////////////////
