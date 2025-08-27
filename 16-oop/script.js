31b3ac41c5229f5dcf6c358e2c08609083798167
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-27 13:43:08 +0600

    what is OOP

---
+++ 16-oop/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * What is OOP?
+ *
+ * We use objects to model real-world or abstract features
+ *
+ * Object may contain data (properties) and code (methods)
+ *
+ * 4 principles of OOP:
+ * 1. Abstraction -> Ignoring or hiding details that don't matter,
+ *  allowing us to get and overview perspective of the thing we're
+ *  implementing, instead of messing with details that don't really
+ *  matter to our implementation.
+ *
+ * 2. Encapsulation -> Keeping properties and methods private inside
+ *  the class, so they are not accessible from outside the class.
+ *  Some methods ca be exposed as a public interface (API)
+ *
+ * 3. Inheritance -> Child class can inherit methods and properties
+ *  prom parent class
+ *
+ * 4. Polymorphism -> A child class can overwrite a method
+ *  it inherited from a parent class.
+ */
