f75eac52b805250efe95beebbfc6024b3636b2cf
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-27 14:04:54 +0600

    constructor function

--- 16-oop/script.js
+++ 16-oop/script.js
@@ -1,23 +1,29 @@
 "use strict";
 
 /**
- * OOP in JS
- *
- * Prototype -> contains methods
- *    |
- * Object -> can access methods
- *
- * Objects are linked to a prototype object;
- *
- * Prototypal inheritance: The prototype contains methods
- * that are accessible to all objects linked to that prototype.
- *
- * Behavior is delegated to the linked prototype
- *
- * How to create new objects:
- * 1. Constructor functions
- *
- * 2. ES6 Classes
- *
- * 3. Object.create()
+ * Constructor functions
  */
+
+// constructor
+const Person = function (firstName, birthYear) {
+  (this.firstName = firstName), (this.birthYear = birthYear);
+
+  // not recommended
+  this.calcAge = function () {
+    console.log(2025 - this.birthYear);
+  };
+};
+
+// instances
+const person1 = new Person("John", 1991);
+console.log(person1);
+
+const person2 = new Person("Bob", 1985);
+console.log(person2);
+
+// checking
+console.log(person2 instanceof Person);
+
+// calling methods
+person1.calcAge();
+person2.calcAge();
