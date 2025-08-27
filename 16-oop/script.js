c583856f8d6293ff0da85b74189ea30fd37a6436
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-27 19:12:46 +0600

    prototypal chaining

--- 16-oop/script.js
+++ 16-oop/script.js
@@ -28,3 +28,20 @@ console.log(Person.prototype);
 // calling method
 person1.calcAge();
 person2.calcAge();
+
+// prototype chaining in builtin objects
+console.log(person1.__proto__.__proto__.__proto__);
+
+const arr = [1, 2, 3, 1, 2, 5, 6, 7, 7, 9, 1];
+console.log(arr.__proto__.__proto__);
+
+// extending builtin objects is not a good idea
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+//
+const h1 = document.querySelector("h1");
+console.dir(h1);
