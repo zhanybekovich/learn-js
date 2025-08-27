32f3cb6bb9403f0cac7d65b8e4ef9771a58dbedd
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-27 19:28:19 +0600

    introduction to ES6 classes

--- 16-oop/script.js
+++ 16-oop/script.js
@@ -1,78 +1,32 @@
 "use strict";
 
 /**
- * Prototypes
+ * ES6 Classes
+ * - Classes are not hoisted
+ * - Classes are 1st class citizens
+ * - Body of the class executed in strict mode
  */
 
-// constructor
-const Person = function (firstName, birthYear) {
-  (this.firstName = firstName), (this.birthYear = birthYear);
-};
+class PersonCl {
+  constructor(firstName, birthYear) {
+    (this.firstName = firstName), (this.birthYear = birthYear);
+  }
 
-// instances
-const person1 = new Person("John", 1991);
+  // methods will be added to .prototype property
+  calcAge() {
+    console.log(2025 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey, this is ${this.firstName}.`);
+  }
+}
+
+const person1 = new PersonCl("John", 1995);
 console.log(person1);
-
-const person2 = new Person("Bob", 1985);
-console.log(person2);
-
-// checking
-console.log(person2 instanceof Person);
-
-// prototype
-Person.prototype.calcAge = function () {
-  console.log(2025 - this.birthYear);
-};
-console.log(Person.prototype);
-
-// calling method
 person1.calcAge();
+person1.greet();
+
+const person2 = new PersonCl("Bob", 2000);
 person2.calcAge();
-
-// prototype chaining in builtin objects
-console.log(person1.__proto__.__proto__.__proto__);
-
-const arr = [1, 2, 3, 1, 2, 5, 6, 7, 7, 9, 1];
-console.log(arr.__proto__.__proto__);
-
-// extending builtin objects is not a good idea
-Array.prototype.unique = function () {
-  return [...new Set(this)];
-};
-
-console.log(arr.unique());
-
-//
-const h1 = document.querySelector("h1");
-console.dir(h1);
-
-// challenge
-const Car = function (make, speed) {
-  (this.make = make), (this.speed = speed);
-};
-
-Car.prototype.accelerate = function () {
-  this.speed += 10;
-  console.log(`${this.make} is going at ${this.speed}`);
-};
-
-Car.prototype.brake = function () {
-  this.speed -= 5;
-  console.log(`${this.make} is going at ${this.speed}`);
-};
-
-const bmw = new Car("BMW", 120);
-bmw.accelerate();
-bmw.accelerate();
-bmw.accelerate();
-bmw.brake();
-bmw.accelerate();
-
-const mercedes = new Car("Mercedes", 95);
-mercedes.accelerate();
-mercedes.brake();
-mercedes.brake();
-mercedes.brake();
-mercedes.brake();
-mercedes.brake();
-mercedes.brake();
+person2.greet();
