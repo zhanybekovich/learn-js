0b525416955eca529166ba16716e61cc5f88ed79
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-28 10:59:42 +0600

    another class example

--- 16-oop/script.js
+++ 16-oop/script.js
@@ -85,3 +85,44 @@ jay.init("Jay", 2010, "Biology");
 
 jay.calcAge();
 jay.introduce();
+
+console.log("=============");
+
+//////////////////////////////
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public interface (API)
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log("Loan is approved!");
+    }
+  }
+}
+
+const acc1 = new Account("John", "EUR", 1111);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(5000);
+console.log(acc1);
