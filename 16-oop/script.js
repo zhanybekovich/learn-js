97f19aa503813bf181b75281577e5930d743c773
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-28 10:12:00 +0600

    inheritance challenge

--- 16-oop/script.js
+++ 16-oop/script.js
@@ -1,30 +1,44 @@
 "use strict";
 
 /**
- * Inheritance
+ * Inheritance challenge
  */
 
-const Person = function (firstName, birthYear) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
 };
 
-Person.prototype.calcAge = function () {
-  console.log(new Date().getFullYear() - this.birthYear);
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
 };
 
-const Student = function (firstName, birthYear, course) {
-  Person.call(this, firstName, birthYear);
-  this.course = course;
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
 };
 
-// linking prototypes
-Student.prototype = Object.create(Person.prototype);
-
-Student.prototype.introduce = function () {
-  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
 };
 
-const mike = new Student("Mike", 2002, "CS");
-mike.introduce();
-mike.calcAge();
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} is going at ${this.speed} km/h with charge of ${this.charge}`
+  );
+};
+const tesla = new EV("Tesla", 120, 25);
+tesla.chargeBattery(90);
+tesla.accelerate();
+tesla.brake();
+console.log(tesla);
