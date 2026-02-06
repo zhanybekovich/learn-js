a51fe3c1415d3c27415f9746d1d700b17cb25310
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-06 18:59:23 +0600

    challenges on string indexes

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 
-// string last index
-let str = "Hello";
-console.log(str.length - 1);
+// challenges on indexes
+let a = "12345";
+console.log(+a[0] + +a[1] + +a[2] + +a[3] + +a[4]);
+
+console.log(a[4] + a[3] + a[2] + a[1] + a[0]);
