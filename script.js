ac0524cca8d5623d7015865416a2764654e5b32f
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-06 18:55:11 +0600

    string last symbol (index)

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 "use strict";
 
-// string index
-let str = "abcde";
-console.log(str[0]);
-console.log(str[1]);
-
-let index = 2;
-console.log(str[index]);
+// string last index
+let str = "Hello";
+console.log(str.length - 1);
