cdb0a9b56041d5b5e8978d3764f5de533c9b7ade
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-06 09:37:36 +0600

    exponentiation has higher priority

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 "use strict";
 
-// exponentiation
-console.log(10 ** 3);
-
-let a = 10;
-let b = 4;
-console.log(a ** b);
-
-console.log(2 ** 10);
+// exponentiation has higher priority
+let a = 3 * 2 ** 3;
+console.log(a);
