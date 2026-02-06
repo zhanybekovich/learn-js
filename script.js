c079c6e0cb28e05bb293538513d5a6959163c253
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-06 21:40:23 +0600

    practice

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 "use strict";
 
-// practice
-console.log(60 * 60 * 24);
+// practice 2
+let r = 20;
+let s = 3.14 * r ** 2;
 
-console.log(60 * 60 * 24 * 30);
+console.log(s);
 
-console.log(60 * 60 * 24 * 365);
+let a = 20;
+s = a * a;
+console.log(s);
 
-console.log(60 * 24);
+a = 25;
+let b = 30;
+console.log(a * b);
 
-console.log(60 * 24 * 365);
+let p = 2 * (a + b);
+console.log(p);
 
-console.log(1024 * 1024);
+let tc = 0;
+let f = (tc * 9) / 5 + 32;
+console.log(f);
 
-console.log(1024 * 1024 * 1024 * 10);
-
-console.log(1028 * 1024 * 1024 * 1024);
-
-console.log((1024 * 1024 * 1024 * 1024) / 1024);
+let tf = 32;
+tc = ((tf - 32) * 5) / 9;
+console.log(tc);
