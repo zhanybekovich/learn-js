1a17a5bab7e5675c93257e43169298e51d422891
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-06 09:38:59 +0600

    strings

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 
-// exponentiation has higher priority
-let a = 3 * 2 ** 3;
-console.log(a);
+// strings
+
+let firstName = "Mirlan";
+let lastName = "Urzhanov";
+console.log(firstName, lastName);
