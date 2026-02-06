ff4ddfa48195de14f850e4d7cfc7d61590d41fd0
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-06 15:34:04 +0600

    short hand conversion to number

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 "use strict";
 
-// convert to number
-let a = "10";
-let b = "20";
-console.log(Number(a) + Number(b));
+// shorthand convert to number
+let a = +"10";
+let b = +"20";
+console.log(a + b);
+
+console.log(+"4" + +"30");
