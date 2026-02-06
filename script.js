f6ff54fe3bb6bf7512782f81dc50a5383f52474b
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-06 21:47:14 +0600

    array length

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
 "use strict";
 
-// array indexes
+// array length
 let letters = ["a", "b", "c", "d", "e"];
 
-console.log(letters[0]);
-console.log(letters[1]);
-
-let numbers = [1, 2, 3, 4, 5];
-console.log(numbers[0]);
-console.log(numbers[1]);
-
-let arr = [1, 2, 3];
-console.log(arr[0] + arr[1] + arr[2]);
-
-arr = ["a", "b", "c", "d"];
-console.log(`${arr[0]}+${arr[1]}+${arr[2]}+${arr[3]}`);
+console.log(letters.length);
+console.log(letters[letters.length - 1]);
