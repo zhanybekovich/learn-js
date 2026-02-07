8f9adf0d56810694b79861e5162bb287acc169b9
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-07 14:31:10 +0600

    and operator has higher priority

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 "use strict";
 
-// logical OR
+// && operator has higher priority over ||
 
-let num = 30;
+let num = 3;
 
-if (num > 0 || num < 10) {
-  console.log("++");
+if ((num > 0 && num < 5) || (num > 10 && num < 20)) {
+  console.log("+++");
 } else {
-  console.log("--");
-}
-
-if (num >= 10 || num <= 20) {
-  console.log("++");
-} else {
-  console.log("--");
+  console.log("---");
 }
