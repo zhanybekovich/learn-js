e256eb952d96e1946cf58d59d7b34182cc2af8b5
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-07 13:52:09 +0600

    arrays and objects are object type

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 "use strict";
 
-// object type
-let user = {
-  name: "John",
-  surname: "Doe",
-};
+// arrays and objects are object types
+let arr = [1, 2, 3];
+let obj = { a: 1, b: 2 };
 
-console.log(typeof user);
+console.log(typeof arr);
+console.log(typeof obj);
 
-console.log(typeof {});
+// array check
+console.log(Array.isArray(arr));
+console.log(Array.isArray(obj));
