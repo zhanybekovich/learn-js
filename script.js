0bd474df3889c580fe4b05acbd4851c29ad9ce85
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-07 12:56:27 +0600

    accessing object values by key

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 "use strict";
 
-// objects with string keys
+// access keys
 let user = {
   name: "John",
   surname: "Doe",
 };
 
-console.log(user);
+console.log(user.name);
+console.log(user.surname);
+
+let date = {
+  year: 2025,
+  month: "January",
+  day: 25,
+};
+
+console.log(date.year);
+console.log(date.month);
+console.log(date.day);
