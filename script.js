d0159d08f2b4b8c340aabbcc9258ecfd13123e78
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-07 19:28:15 +0600

    practice with arrays

--- script.js
+++ script.js
@@ -2,24 +2,17 @@
 
 // loops: for with arrays
 
-let arr = [1, 2, 3, 4, 5];
+let arr = [2, 5, 9, 15, 1, 4];
 
-for (let i = 0; i <= arr.length - 1; i++) {
-  console.log(arr[i]);
-}
-
-for (let i = 0; i <= arr.length - 1; i++) {
-  if (i !== 0 && i !== arr.length - 1) {
-    console.log(arr[i]);
+for (let x of arr) {
+  if (x > 3 && x < 10) {
+    console.log(x);
   }
 }
 
-for (let i = arr.length - 1; i >= 0; i--) {
-  console.log(arr[i]);
-}
-
-arr = ["a", "b", "c", "d", "e"];
-
-for (let i = 0; i <= arr.length - 1; i++) {
-  console.log(arr[i]);
+let obj = { a: 1, b: 2, c: 3, d: 4, e: 5 };
+for (let key in obj) {
+  if (obj[key] % 2 !== 0) {
+    console.log(obj[key]);
+  }
 }
