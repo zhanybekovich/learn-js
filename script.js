5a8dce540498b955d61a186d5f4e40a4edbb21b0
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-07 14:15:02 +0600

    logical or

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 "use strict";
 
-// logical AND
+// logical OR
 
 let num = 30;
 
-if (num > 0 && num < 10) {
+if (num > 0 || num < 10) {
   console.log("++");
 } else {
   console.log("--");
 }
 
-if (num >= 10 && num <= 20) {
+if (num >= 10 || num <= 20) {
   console.log("++");
 } else {
   console.log("--");
