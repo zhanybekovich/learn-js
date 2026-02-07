8089c284d5562c869c53c7d49c6483379cbfdca3
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-07 13:59:06 +0600

    reference types

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 "use strict";
 
-// primitive types: string, number, boolean, null, undefined, symbol, bigint
+// reference types
+
+let obj1 = { a: 1, b: 2, c: 3 };
+let obj2 = obj1;
+
+console.log(obj1);
+console.log(obj2);
+
+obj2.a = 200;
+
+console.log(obj1);
+console.log(obj2);
+
+let arr1 = [1, 2, 3];
+let arr2 = arr1;
+
+arr1[0] = "a";
+console.log(arr1);
+console.log(arr2);
