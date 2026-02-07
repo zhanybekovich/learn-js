05504d0428c14df411795a60c1b65bd5a6e4d887
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-07 15:52:38 +0600

    else if

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 "use strict";
 
-// not operator
+// else if
+let n = 5;
 
-let test = false;
-
-if (!test) {
-  console.log("+++");
+if (n == 1) {
+  console.log(1);
+} else if (n == 2) {
+  console.log(2);
+} else if (n == 3) {
+  console.log(3);
 } else {
-  console.log("---");
+  console.log("Invalid value");
+}
+
+let day = 15;
+
+if (day <= 10) {
+  console.log("1st");
+} else if (day > 10 && day <= 20) {
+  console.log("2nd");
+} else if (day > 20) {
+  console.log("3rd");
 }
