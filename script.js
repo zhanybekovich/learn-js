2f72f960418ba79191fb44be15aadf0638da9b61
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-07 19:15:25 +0600

    while loop

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 "use strict";
 
-// loops: for-in
-let user = {
-  name: "John",
-  email: "john@john.com",
-  city: "London",
-};
+// loops: while
 
-for (let key in user) {
-  console.log(`${key}: ${user[key]}`);
+let i = 1;
+
+while (i <= 100) {
+  console.log(i);
+  i++;
 }
+
+i = 11;
+
+while (i <= 33) {
+  console.log(i);
+  i++;
+}
+
+i = 100;
+
+while (i >= 1) {
+  console.log(i);
+  i--;
+}
+
+let n = 2;
+let count = 0;
+
+while (n <= 1000) {
+  n = n * 3;
+  count++;
+}
+
+console.log(count);
