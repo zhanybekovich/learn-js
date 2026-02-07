7af0784b95741068dff399b2e0fb00f9228377c2
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-07 12:53:39 +0600

    objects 2

--- script.js
+++ script.js
@@ -2,9 +2,18 @@
 
 // objects
 let obj = {
-  1: 1,
-  2: 2,
-  3: 3,
+  1: "January",
+  2: "February",
+  3: "March",
+  4: "April",
+  5: "May",
+  6: "June",
+  7: "July",
+  8: "August",
+  9: "September",
+  10: "October",
+  11: "November",
+  12: "December",
 };
 
 console.log(obj);
