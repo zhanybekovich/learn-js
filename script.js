d18c1c46aa7ec43550a5abd65473384a14d0f2d9
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-07 18:45:55 +0600

    practice

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 "use strict";
 
 // practice
-// let num1 = 1;
-// let num2 = 2;
+let month = 12;
 
-// if (num1 + num2 === 3) {
-//   console.log("+++");
-// } else {
-//   console.log("---");
-// }
-
-let num1 = "1";
-let num2 = "2";
-
-if (num1 + num2 === 3) {
-  console.log("+++");
+if (month === 12 || month === 1 || month === 2) {
+  console.log("Winter");
+} else if (month >= 3 && month <= 5) {
+  console.log("Spring");
+} else if (month >= 6 && month <= 8) {
+  console.log("Summer");
+} else if (month >= 9 && month <= 11) {
+  console.log("Autumn");
 } else {
-  console.log("---");
+  console.log("Unknown moth");
 }
+
+let str = "abcde";
+
+str[0] === "a" ? console.log(true) : console.log(false);
+
+let x = 12345;
+Number(String(x)[0]) === 1 ||
+Number(String(x)[0]) === 2 ||
+Number(String(x)[0]) === 3
+  ? console.log("1 or 2 or 3")
+  : console.log("not");
+
+let y = 234;
+console.log(Number(String(y)[0]) + Number(String(y)[1]) + Number(String(y)[2]));
+
+let z = 123456;
+Number(String(z)[0]) + Number(String(z)[1]) + Number(String(z)[2]) ===
+Number(String(z)[3]) + Number(String(z)[4]) + Number(String(z)[5])
+  ? console.log("===")
+  : console.log("!==");
