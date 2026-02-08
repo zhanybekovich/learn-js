302699efdf312d398a50e22440ebd52c6e5e11ac
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-08 16:35:18 +0600

    practice

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 "use strict";
 
 // practice
-let employees = [
-  {
-    name: "name1",
-    salary: 300,
-    age: 28,
-  },
-  {
-    name: "name2",
-    salary: 400,
-    age: 29,
-  },
-  {
-    name: "name3",
-    salary: 500,
-    age: 30,
-  },
-];
+// let affairs = {
+//   "2019-12-28": ["data11", "data12", "data13"],
+//   "2019-12-29": ["data21", "data22", "data23"],
+//   "2019-12-30": ["data31", "data32", "data33"],
+// };
 
-employees.push({ name: "John", salary: 200, age: 34 });
-console.log(employees);
+// affairs["2019-12-29"] = [];
+// affairs["2019-12-29"].push("data34", "data35", "data36");
+
+// console.log(affairs);
+
+let students = {
+  group1: {
+    subgroup11: ["student111", "student112", "student113"],
+    subgroup12: ["student121", "student122", "student123"],
+  },
+  group2: {
+    subgroup21: ["student211", "student212", "student213"],
+    subgroup22: ["student221", "student222", "student223"],
+  },
+  group3: {
+    subgroup31: ["student311", "student312", "student313"],
+    subgroup32: ["student321", "student322", "student323"],
+  },
+};
+
+students.group1.subgroup11.push("student114");
+console.log(students);
