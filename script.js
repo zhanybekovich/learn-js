326b6a302f500eb0a343c608804f62df4784f2bd
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-08 16:31:31 +0600

    practice

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
 "use strict";
 
 // practice
-let affairs = {
-  2018: {
-    11: {
-      29: ["name111", "name112", "name113"],
-      30: ["name121", "name122", "name123"],
-    },
-    12: {
-      30: ["name211", "name212", "name213"],
-      31: ["name221", "name222", "name223"],
-    },
+let employees = [
+  {
+    name: "name1",
+    salary: 300,
+    age: 28,
   },
-  2019: {
-    12: {
-      29: ["name311", "name312", "name313"],
-      30: ["name321", "name322", "name323"],
-      31: ["name331", "name332", "name333"],
-    },
+  {
+    name: "name2",
+    salary: 400,
+    age: 29,
   },
-};
+  {
+    name: "name3",
+    salary: 500,
+    age: 30,
+  },
+];
 
-let year = 2019;
-let month = 12;
-let day = 29;
-
-console.log(affairs[year][month][day]);
+employees.push({ name: "John", salary: 200, age: 34 });
+console.log(employees);
