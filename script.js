47814f0a789a3c90cfcae2281aaa0628c992c179
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-08 11:41:13 +0600

    practice

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 "use strict";
 
 // practice
-let arr = [10, 20, 30, 40, 21, 32, 51];
+let obj = { a: 10, b: 20, c: 30, d: 40, e: 50 };
 let sum = 0;
 
-for (let el of arr) {
-  if (String(el)[0] == 1 || String(el)[0] == 2) {
-    sum += el;
+for (let elem in obj) {
+  if (String(obj[elem])[0] === "1" || String(obj[elem])[0] === "2") {
+    sum += obj[elem];
   }
 }
+
 console.log(sum);
