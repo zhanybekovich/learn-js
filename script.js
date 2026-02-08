f5323bd235267bba260c4558d0459dd9ad6aa168
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-08 20:45:08 +0600

    practice

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 "use strict";
 
 // practice
-// function func(a, b) {
-//   return a === b;
+// function func1() {
+//   return 3;
+// }
+// function func2() {
+//   return 5;
 // }
 
-// console.log(func(2, 2));
-// console.log(func(2, 3));
+// console.log(func1() + func2());
 
-function func(a, b) {
-  return a != b;
-}
+// function sum(arr) {
+//   let res = 0;
 
-console.log(func(2, 1));
-console.log(func(2, 2));
+//   for (let elem of arr) {
+//     res += elem;
+//   }
+
+//   return res;
+// }
+
+// console.log(sum([1, 2, 3, 4, 5]));
+
+// let arr = [1, 2, 3, 4, 5];
+
+// function func(arr) {
+//   let res = 0;
+
+//   for (let elem of arr) {
+//     res += elem;
+//   }
+
+//   return res;
+// }
+// console.log(func(arr));
