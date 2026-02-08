1068749c52e1544b10cb0f599a40fc73ef8bf168
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-08 16:02:51 +0600

    practice

--- script.js
+++ script.js
@@ -2,47 +2,29 @@
 
 // practice
 let obj = {
-  key1: {
-    key1: 1,
-    key2: 2,
-    key3: 3,
+  1: {
+    1: 11,
+    2: 12,
+    3: 13,
   },
-  key2: {
-    key1: 4,
-    key2: 5,
-    key3: 6,
+  2: {
+    1: 21,
+    2: 22,
+    3: 23,
   },
-  key3: {
-    key1: 7,
-    key2: 8,
-    key3: 9,
+  3: {
+    1: 24,
+    2: 25,
+    3: 26,
   },
 };
-let sum = 0;
 
+let sum = 0;
 for (let key in obj) {
-  for (let sub in obj[key]) {
-    sum += obj[key][sub];
+  let subObj = obj[key];
+
+  for (let subKey in subObj) {
+    sum += subObj[subKey];
   }
 }
 console.log(sum);
-
-obj = {
-  1: {
-    1: "a1",
-    2: "a2",
-    3: "a3",
-  },
-  2: {
-    1: "b1",
-    2: "b2",
-    3: "b3",
-  },
-  3: {
-    1: "c1",
-    2: "c2",
-    3: "c3",
-  },
-};
-console.log(obj["2"]["2"]);
-console.log(obj["3"][1]);
