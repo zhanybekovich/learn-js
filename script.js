6098b16d35d3dd2ac3be042b5156357434b15e9e
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-08 10:10:02 +0600

    practice

--- script.js
+++ script.js
@@ -1,30 +1,14 @@
 "use strict";
 
 // practice
-const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
-const dayNumbers = [1, 2, 3, 4, 5, 6, 7];
-
-const weekDays = {};
-
-for (let i = 0; i <= dayNumbers.length - 1; i++) {
-  weekDays[i] = days[i];
-}
-
-console.log(weekDays);
-
-let obj = { a: 1, b: 2, c: 3, d: 4, e: 5 };
-let newObj = {};
+let obj = { x: 1, y: 2, z: 3 };
 
 for (let key in obj) {
-  if (obj[key] % 2 === 0) {
-    newObj[key] = obj[key];
-  }
+  obj[key] = obj[key] * obj[key];
 }
-console.log(newObj);
-
-let newObj2 = {};
+console.log(obj);
 
 for (let key in obj) {
-  newObj2[obj[key]] = key;
+  obj[key] = obj[key] + 1;
 }
-console.log(newObj2);
+console.log(obj);
