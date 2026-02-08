86e1e5d60e253c7b1fdc6ff19737d589586f0e13
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-08 20:00:41 +0600

    practice

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 "use strict";
 
-// function with several params
+// practice
 
 function calcSum(a, b, c) {
   console.log(a + b + c);
 }
 
 calcSum(1, 2, 3);
+
+let param1 = 1;
+let param2 = 2;
+let param3 = 3;
+calcSum(param1, param2, param3);
