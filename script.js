276219869048b3776894599619ef28d55e6496b3
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-08 16:26:58 +0600

    practice

--- script.js
+++ script.js
@@ -1,62 +1,78 @@
 "use strict";
 
 // practice
-// let data = {
-//   1: ["data11", "data12", "data13"],
-//   2: ["data21", "data22", "data23"],
-//   3: ["data31", "data32", "data33"],
-//   4: ["data41", "data42", "data43"],
-// };
-
-// for (let key in data) {
-//   for (let item of data[key]) {
-//     console.log(item);
-//   }
-// }
-
-// let data = [
+// let employees = [
 //   {
-//     1: "data11",
-//     2: "data12",
-//     3: "data13",
+//     name: "name1",
+//     salary: 300,
 //   },
 //   {
-//     1: "data21",
-//     2: "data22",
-//     3: "data33",
+//     name: "name2",
+//     salary: 400,
 //   },
 //   {
-//     1: "data31",
-//     2: "data32",
-//     3: "data33",
+//     name: "name3",
+//     salary: 500,
 //   },
 // ];
-// for (let item of data) {
-//   for (let key in item) {
-//     console.log(item[key]);
-//   }
+
+// for (let item of employees) {
+//   console.log(`${item.name} - ${item.salary}`);
 // }
 
-let data = [
+// let employees = [
+//   {
+//     name: "name1",
+//     salary: 300,
+//   },
+//   {
+//     name: "name2",
+//     salary: 400,
+//   },
+//   {
+//     name: "name3",
+//     salary: 500,
+//   },
+// ];
+// let sum = 0;
+
+// for (let item of employees) {
+//   sum += item.salary;
+// }
+// console.log(sum);
+
+let employees = [
   {
-    1: ["data111", "data112", "data113"],
-    2: ["data121", "data122", "data123"],
+    name: "name1",
+    salary: 300,
+    age: 28,
   },
   {
-    1: ["data211", "data212", "data213"],
-    2: ["data221", "data222", "data223"],
+    name: "name2",
+    salary: 400,
+    age: 29,
   },
   {
-    1: ["data411", "data412", "data413"],
-    2: ["data421", "data422", "data423"],
+    name: "name3",
+    salary: 500,
+    age: 30,
+  },
+  {
+    name: "name4",
+    salary: 600,
+    age: 31,
+  },
+  {
+    name: "name5",
+    salary: 700,
+    age: 32,
   },
 ];
+let sum = 0;
 
-for (let item of data) {
-  const obj = item;
-  for (let key in obj) {
-    for (let item of obj[key]) {
-      console.log(item);
-    }
+for (let item of employees) {
+  if (item.age >= 30) {
+    sum += item.salary;
   }
 }
+console.log(sum);
