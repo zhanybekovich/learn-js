b615c47f835612b2672f3cd7dca1913abec5191c
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-08 16:38:15 +0600

    Math module

--- script.js
+++ script.js
@@ -1,31 +1,13 @@
 "use strict";
 
-// practice
-// let affairs = {
-//   "2019-12-28": ["data11", "data12", "data13"],
-//   "2019-12-29": ["data21", "data22", "data23"],
-//   "2019-12-30": ["data31", "data32", "data33"],
-// };
+// Math module
+console.log(Math.pow(2, 10));
+console.log(Math.sqrt(245));
 
-// affairs["2019-12-29"] = [];
-// affairs["2019-12-29"].push("data34", "data35", "data36");
+const arr = [4, 2, 5, 19, 13, 0, 10];
+let sum = 0;
 
-// console.log(affairs);
-
-let students = {
-  group1: {
-    subgroup11: ["student111", "student112", "student113"],
-    subgroup12: ["student121", "student122", "student123"],
-  },
-  group2: {
-    subgroup21: ["student211", "student212", "student213"],
-    subgroup22: ["student221", "student222", "student223"],
-  },
-  group3: {
-    subgroup31: ["student311", "student312", "student313"],
-    subgroup32: ["student321", "student322", "student323"],
-  },
-};
-
-students.group1.subgroup11.push("student114");
-console.log(students);
+for (let n of arr) {
+  sum += Math.sqrt(n * n * n);
+}
+console.log(sum);
