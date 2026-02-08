e23fd2b6cbf103f237e3cb3daa7411f2f454f605
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-08 17:05:50 +0600

    math random

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 
-// Math max & min
-console.log(Math.max(4, -2, 5, 19, -130, 0, 10));
-console.log(Math.min(4, -2, 5, 19, -130, 0, 10));
+// Math random
+console.log(Math.random());
+
+// random in range 1 (including) to 5 (including)
+console.log(Math.floor(Math.random() * (5 - 1 + 1) + 1));
