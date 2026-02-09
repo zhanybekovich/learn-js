c4536d17712ee959a2fd4eb1e76fb6ffac43b6bf
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-09 20:44:46 +0600

    practice spread

--- script.js
+++ script.js
@@ -3,8 +3,4 @@
 // spread operator (ES6)
 const arr = [1, 2, 3, 4, 5];
 
-function func(num1, num2, num3, num4, num5) {
-  return num1 + num2 + num3 + num4 + num5;
-}
-
-console.log(func(...arr));
+console.log(Math.min(...arr));
