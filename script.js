9d23310d6ac9567c0db9be149edcdbfb7d08b679
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-09 11:48:00 +0600

    arrow function

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
 "use strict";
 
-// callback
-function each(arr, callback) {
-  const result = [];
-  for (let item of arr) {
-    result.push(callback(item));
-  }
-
-  return result;
-}
-
-console.log(
-  each([1, 2, 3, 4, 5], function (n) {
-    return (n += 1);
-  }),
-);
-
-function increment(n) {
-  return (n += 1);
-}
-
-console.log(each([1, 2, 3, 4, 5], increment));
+// arrow function
+const sayHello = () => console.log("hello");
+sayHello();
