e54635d5e16e35d1e77de6a6bdefe54e22be36fb
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-09 10:42:13 +0600

    practice

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 "use strict";
 
-// function expression
-let func = function () {
-  console.log("Hello");
-};
-
-func();
+// practice
+// 1
+const arr = [
+  function () {
+    return 1;
+  },
+  function () {
+    return 2;
+  },
+  function () {
+    return 3;
+  },
+];
+for (const func of arr) {
+  console.log(func());
+}
