79309033864b7b9efe24f645ee8ad85f38f2fa41
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-09 12:28:59 +0600

    immediately invoked function expression

--- script.js
+++ script.js
@@ -1,30 +1,6 @@
 "use strict";
 
-// closure practice
-function test() {
-  let num = 3;
-
-  return function () {
-    if (num === 0) {
-      console.log("Countdown finished!");
-    } else {
-      console.log(num);
-      num--;
-    }
-  };
-}
-
-let func = test();
-func();
-func();
-
-let anotherFunc = test();
-anotherFunc();
-anotherFunc();
-
-func();
-anotherFunc();
-
-func();
-func();
-anotherFunc();
+// Immediately Invoked Function Expression
++(function () {
+  console.log("Hello");
+})();
