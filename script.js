ac83b677c46c806bf62d68d160813f4efb432ec1
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-09 11:09:53 +0600

    practice

--- script.js
+++ script.js
@@ -2,18 +2,29 @@
 
 // practice
 // 1
-const obj = {
-  func1: function () {
-    return 1;
+const math = {
+  sum: function (arr) {
+    let sum = 0;
+    for (let x of arr) {
+      sum += x;
+    }
+    return sum;
   },
-  func2: function () {
-    return 2;
+  square: function (arr) {
+    let sum = 0;
+    for (let x of arr) {
+      sum += x * x;
+    }
+    return sum;
   },
-  func3: function () {
-    return 3;
+  cube: function (arr) {
+    let sum = 0;
+    for (let x of arr) {
+      sum += x * x * x;
+    }
+    return sum;
   },
 };
-
-for (const func in obj) {
-  console.log(obj[func]());
-}
+console.log(math.sum([1, 2, 3]));
+console.log(math.square([1, 2, 3]));
+console.log(math.cube([1, 2, 3]));
