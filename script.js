6284e1592cea9c5a29602513b2c47bba0c8baa40
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-09 20:41:01 +0600

    array reduceRight method

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// reduce
+// reduceRight
 const arr = [1, 2, 3, 4, 5, 6];
-const sumOfArr = arr.reduce((sum, elem) => sum + elem, 0);
+const sumOfArr = arr.reduceRight((sum, elem) => sum + elem, 0);
 console.log(sumOfArr);
