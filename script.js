15d30fdc3d4dac2dd29a36973fb5a6a8ababc273
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-09 13:02:59 +0600

    array map method

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 "use strict";
 
-// recursion
-let arr = [1, 2, 3, 4, 5];
+// map
+// 1
+const numbers = [1, 2, 3, 4, 5];
+const sqrts = numbers.map((x) => Math.sqrt(x));
+console.log(sqrts);
 
-function func(arr) {
-  console.log(arr.shift(), arr);
+// 2
+const names = ["john", "pete", "bob", "patrick"];
+const transformedNames = names.map((name) => name + "!");
+console.log(transformedNames);
 
-  if (arr.length != 0) {
-    func(arr);
-  }
-}
+// 3
+const names2 = ["john", "pete", "bob", "patrick"];
+const reversedNames = names2.map((name) => name.split("").reverse().join(""));
+console.log(reversedNames);
 
-func(arr);
+// 4
+const arr = ["123", "456", "789"];
+const transformedArr = arr.map((item) => item.split("").map((item) => +item));
+console.log(transformedArr);
+
+// 5
+const numbers2 = [1, 2, 3, 4, 5];
+const transformedNumbers = numbers2.map((item, index) => item * index);
+console.log(transformedNumbers);
