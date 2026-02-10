d725fd311b8d4768286662eb299e97a129d10285
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-10 21:01:03 +0600

    innerHTML

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
 "use strict";
 
 // DOM
-const btn = document.querySelector("#btn");
 const p = document.querySelector("p");
+const readHTMLBtn = document.querySelector("#btn1");
+const makeCursiveBtn = document.querySelector("#btn2");
 
-btn.addEventListener("click", function () {
-  console.log(p.textContent);
+readHTMLBtn.addEventListener("click", function () {
+  console.log(p.innerHTML);
 });
 
-const sumBtn = document.querySelector("#sum");
-const n1 = document.querySelector("#num1");
-const n2 = document.querySelector("#num2");
-const n3 = document.querySelector("#num3");
-const result = document.querySelector("#result");
-
-sumBtn.addEventListener("click", function () {
-  result.textContent =
-    Number(n1.textContent) + Number(n2.textContent) + Number(n3.textContent);
-});
-
-const textP = document.querySelector(".text");
-const addExBtn = document.querySelector("#addExclamation");
-
-addExBtn.addEventListener("click", function () {
-  textP.textContent += "!";
+makeCursiveBtn.addEventListener("click", function () {
+  const text = p.textContent;
+  p.innerHTML = `<i>${text}</i>`;
 });
