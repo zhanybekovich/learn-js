e2921eef112ebd0bc55a43632980a3272c302f3a
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-10 19:07:02 +0600

    set date

--- script.js
+++ script.js
@@ -1,27 +1,8 @@
 "use strict";
 
-// Date object
-
-const date = new Date();
+// set date
+const date = new Date(2026, 1, 9, 12, 34, 20);
 console.log(date);
 
-// year
-console.log(date.getFullYear());
-
-// month
-console.log(date.getMonth());
-
-// day of month
-console.log(date.getDate());
-
-// day of the week
-console.log(date.getDay());
-
-// hours
-console.log(date.getHours());
-
-// minutes
-console.log(date.getMinutes());
-
-// seconds
-console.log(date.getSeconds());
+const myBirthday = new Date(1985, 0, 24);
+console.log(myBirthday);
