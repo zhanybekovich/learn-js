9c0834574872200d0eb2e0892c914fc87b68d013
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-11 12:24:30 +0600

    removeEventListener

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 "use strict";
 
-// querySelectorAll
-const paragraphs = document.querySelectorAll("p");
+// removeEventListener
+const link = document.querySelector("a");
+const btn1 = document.querySelector("#btn1");
 
-paragraphs.forEach((p) =>
-  p.addEventListener("click", function () {
-    p.textContent = +p.textContent * +p.textContent;
-  }),
-);
+function func() {
+  link.textContent += `(${link.href})`;
+  this.removeEventListener("click", func);
+}
+
+btn1.addEventListener("click", func);
+
+const btn2 = document.querySelector("#btn2");
+
+function increment() {
+  let current = +btn2.textContent;
+  btn2.textContent = current + 1;
+
+  if (+btn2.textContent === 10) {
+    btn2.removeEventListener("click", increment);
+  }
+}
+
+btn2.addEventListener("click", increment);
