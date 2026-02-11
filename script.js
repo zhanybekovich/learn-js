6c5de48cfba3e2b62a727e6779f627611939315c
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-11 13:28:35 +0600

    getAttribute

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 "use strict";
 
-// practice
-const input = document.querySelector("input");
-const p = document.querySelector("p");
-
-input.addEventListener("blur", () => {
-  const numbers = input.value.split(",").map((item) => +item.trim());
-  p.textContent = numbers.reduce((sum, n) => sum + n, 0) / numbers.length;
-});
+// getAttribute
+console.log(document.querySelector("input").getAttribute("value"));
+console.log(document.querySelector("input").getAttribute("class"));
