1693b5639298b58bf2842d32ef3c04a1f2abcf89
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-11 12:43:55 +0600

    practice input & p

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
 "use strict";
 
-// removeEventListener
-const link = document.querySelector("a");
-const btn1 = document.querySelector("#btn1");
+// practice
+const input = document.querySelector("input");
+const p = document.querySelector("p");
 
-function func() {
-  link.textContent += `(${link.href})`;
-  this.removeEventListener("click", func);
-}
-
-btn1.addEventListener("click", func);
-
-const btn2 = document.querySelector("#btn2");
-
-function increment() {
-  let current = +btn2.textContent;
-  btn2.textContent = current + 1;
-
-  if (+btn2.textContent === 10) {
-    btn2.removeEventListener("click", increment);
-  }
-}
-
-btn2.addEventListener("click", increment);
+input.addEventListener("blur", function () {
+  p.textContent += " " + this.value;
+});
