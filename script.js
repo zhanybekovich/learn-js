1a94b049793b2a5a10b53bbbf4fd74aba5229a07
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-11 10:08:41 +0600

    getting value from input

--- script.js
+++ script.js
@@ -1,27 +1,10 @@
 "use strict";
 
 // DOM
-const textP = document.querySelector("p");
-console.log(textP.id);
+const input = document.querySelector("#input");
+const showValueBtn = document.querySelector("#showValue");
+const p = document.querySelector("p");
 
-const input = document.querySelector("input");
-console.log(input.id);
-console.log(input.type);
-
-// change attribute
-input.type = "submit";
-input.value = "Send";
-
-const link = document.querySelector("a");
-const showHrefBtn = document.querySelector("#showHref");
-
-showHrefBtn.addEventListener("click", function () {
-  link.textContent = `(${link.href})`;
-});
-
-const img = document.querySelector("img");
-const changeSizeBtn = document.querySelector("#changeSize");
-
-changeSizeBtn.addEventListener("click", function () {
-  img.width = 200;
+showValueBtn.addEventListener("click", function () {
+  p.textContent = input.value;
 });
