bc1feea6afe31c4070c3c697a207fe68798397ac
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-11 12:01:12 +0600

    practice querySelectorAll with event handlers

--- script.js
+++ script.js
@@ -2,18 +2,9 @@
 
 // querySelectorAll
 const paragraphs = document.querySelectorAll("p");
-const btn = document.querySelector("button");
 
-btn.addEventListener("click", function () {
-  paragraphs.forEach((item, index) => (item.textContent += index));
-});
-
-const inputs = document.querySelectorAll("input");
-const sumBtn = document.querySelector("#get-sum");
-const resultP = document.querySelector("#result");
-
-sumBtn.addEventListener("click", function () {
-  let sum = 0;
-  inputs.forEach((n) => (sum += Number(n.value)));
-  resultP.textContent = sum;
-});
+paragraphs.forEach((p) =>
+  p.addEventListener("click", function () {
+    p.textContent = +p.textContent * +p.textContent;
+  }),
+);
