edf6dc852a64c9e5cfc29a00330f12ce95c9d664
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-11 18:45:40 +0600

    querySelectorAll vs etElementsByTagName

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 "use strict";
 
-// getElementById
+// querySelectorAll vs etElementsByTagName
 
-const elem = document.getElementById("elem");
+// returns NodeList
+const items = document.querySelectorAll("li");
+console.log(items);
+items.forEach((item) => (item.style.color = "green"));
 
-elem.textContent = "Hello!";
+// returns HTMLCollection
+const items2 = document.getElementsByTagName("li");
+console.log(items2);
+
+// forEach won't work with HTMLCollection: transform to array before
+[...items2].forEach((item) => (item.style.color = "blue"));
