76f4c19850fa3aa1792843fd4b65b23ea57dc364
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-11 18:19:43 +0600

    styling in js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 "use strict";
 
-// classList
+// style
+const btn = document.querySelector("button");
+const div = document.querySelector("div");
 
-document.querySelector("p").classList.toggle("zzz");
+btn.addEventListener("click", function () {
+  div.style.width = "300px";
+  div.style.height = "300px";
+  div.style.border = "1px solid blue";
+  div.style.margin = "30px 0";
+  div.style.backgroundColor = "green";
+});
