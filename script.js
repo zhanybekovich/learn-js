777cda37897348d17cd601cf283ee3aebc1287ae
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-12 12:43:36 +0600

    nodeType

--- script.js
+++ script.js
@@ -6,5 +6,8 @@ const elem = document.querySelector("#elem");
 console.log(elem.childNodes);
 
 for (let item of elem.childNodes) {
-  console.log(item.nodeName);
+  console.log(item.nodeType);
+  // 1 - element
+  // 3 - text
+  // 8 - comments
 }
