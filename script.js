7f49bd9fa035616faed9015652aebd555bdf6a51
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-12 20:57:07 +0600

    input change event

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 "use strict";
 
-// radio
-const radios = document.querySelectorAll("input");
-const btn = document.querySelector("button");
+// change event
+const input = document.querySelector("input");
 const p = document.querySelector("p");
 
-btn.addEventListener("click", function () {
-  for (let radio of radios) {
-    if (radio.checked) {
-      p.textContent = radio.value;
-    }
-  }
+input.addEventListener("change", function () {
+  console.log(this.value);
 });
