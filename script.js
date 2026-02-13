89795ba0480505ead86b1c501f3d8a691f62d1f0
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-13 18:47:34 +0600

    setTimeout

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 "use strict";
 
-// practice
+// setTimeout
 
 const p = document.querySelector("p");
 
-setInterval(() => {
-  const time = new Date();
-  p.textContent = `${String(time.getHours()).padStart(2, "0")}:${String(time.getMinutes()).padStart(2, "0")}:${String(time.getSeconds()).padStart(2, "0")}`;
-}, 1000);
+setTimeout(() => {
+  p.textContent = "Hello";
+}, 2000);
