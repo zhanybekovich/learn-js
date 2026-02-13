28d3cdc55120f183706fdd894fe376ede5b72d68
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-13 11:36:02 +0600

    practice with select

--- script.js
+++ script.js
@@ -1,26 +1,7 @@
 "use strict";
 
 // working with select
-const select = document.querySelector("#select");
-const btn = document.querySelector("button");
-const p = document.querySelector("p");
+const select = document.querySelector("select");
+const currentMonth = new Date().getMonth();
 
-btn.addEventListener("click", function () {
-  let selectedDay = +select.value;
-  switch (selectedDay) {
-    case 1:
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-      p.textContent = "Working day!";
-      break;
-    case 6:
-    case 7:
-      p.textContent = "Weekend!";
-      break;
-    default:
-      p.textContent = "Unknown day";
-      break;
-  }
-});
+select.value = currentMonth + 1;
