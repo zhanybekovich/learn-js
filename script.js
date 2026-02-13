92869287b09d7cfa67d648f510f731d05cc2fffb
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-13 10:01:45 +0600

    practice with select

--- script.js
+++ script.js
@@ -6,5 +6,21 @@ const btn = document.querySelector("button");
 const p = document.querySelector("p");
 
 btn.addEventListener("click", function () {
-  p.textContent = select.value;
+  let selectedDay = +select.value;
+  switch (selectedDay) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+      p.textContent = "Working day!";
+      break;
+    case 6:
+    case 7:
+      p.textContent = "Weekend!";
+      break;
+    default:
+      p.textContent = "Unknown day";
+      break;
+  }
 });
