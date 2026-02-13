8fd66158f58c11c8125609798479dafd7d5a980d
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-13 14:41:18 +0600

    preventDefault

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 "use strict";
 
-// keypress
-const input = document.querySelector("input");
-const p = document.querySelector("p");
-
-p.addEventListener("click", function (e) {
-  if (e.altKey) {
-    this.style.border = "2px solid green";
-  }
+// preventDefault
+document.querySelector("a").addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("Hello");
 });
