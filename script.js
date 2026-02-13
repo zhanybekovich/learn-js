9b978fedc1bc7735b012a0a57b6bd5b418c0a8b5
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-13 13:14:13 +0600

    event target

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 "use strict";
 
 // event object
-const elem = document.querySelector("#elem");
-elem.addEventListener("click", func);
-elem.addEventListener("dblclick", func);
-
-function func(e) {
-  if (e.type === "click") {
-    elem.style.background = "green";
-  }
-
-  if (e.type === "dblclick") {
-    elem.style.background = "blue";
-  }
-}
+document.querySelector("button").addEventListener("click", function (e) {
+  console.log(e.target);
+});
