7ea32fc6d1244f9ed69b7ec68b975dd42e5c237e
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-14 12:30:20 +0600

    practice

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
 "use strict";
 
 // practice
-const items = document.querySelectorAll("li");
+const btns = document.querySelectorAll("button");
 
-for (let item of items) {
-  item.addEventListener("click", function func() {
-    const input = document.createElement("input");
-    input.type = "text";
-    input.value = item.textContent;
-
-    item.textContent = "";
-    item.appendChild(input);
-    input.focus();
-
-    input.addEventListener("blur", function () {
-      item.textContent = this.value;
-      item.addEventListener("click", func);
-    });
-
-    item.removeEventListener("click", func);
+btns.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    e.target.previousElementSibling.classList.toggle("hide");
   });
-}
+});
