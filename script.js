929bfff964bd308dc0d358f7d0640084c970ac47
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-14 15:37:23 +0600

    set styles by cssText

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 "use strict";
 
 const div = document.querySelector("div");
-const hide = document.querySelector("#color");
+const hide = document.querySelector("#style");
 const show = document.querySelector("#remove-color");
 
 hide.addEventListener("click", () => {
-  div.style.backgroundColor = "red";
-});
-
-show.addEventListener("click", () => {
-  div.style.backgroundColor = "initial";
+  div.style.cssText = `
+    width: 100px;
+    height: 100px;
+    background-color: green;
+  `;
 });
