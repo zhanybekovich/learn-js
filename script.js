57100479cde526809b0ad525f65c905f56f15f0c
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-14 15:25:51 +0600

    get element styles

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const div = document.querySelector("div");
 const btn = document.querySelector("button");
 
 btn.addEventListener("click", () => {
-  div.style.width = "400px";
-  div.style.height = "300px";
-  div.style.border = "1px solid blue";
+  console.log(parseInt(div.style.width));
+  console.log(parseInt(div.style.height));
 });
