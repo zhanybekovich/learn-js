97720012a9f657b0e8753cd523d3a826264594fe
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2026-02-14 15:21:00 +0600

    calc area and perimeter of square

--- script.js
+++ script.js
@@ -1,53 +1,10 @@
 "use strict";
 
-// practice
-const employees = [
-  { name: "employee1", age: 30, salary: 400 },
-  { name: "employee2", age: 31, salary: 500 },
-  { name: "employee3", age: 32, salary: 600 },
-];
+// create calculator that returns area and perimeter of square
 
-const table = document.querySelector("table");
-table.border = "1";
-
-const headingRow = document.createElement("tr");
-const keys = Object.keys(employees[0]);
-
-for (let key of keys) {
-  const th = document.createElement("th");
-  th.textContent = key.toUpperCase();
-
-  headingRow.appendChild(th);
+function calcAreaAndPerimeter(a) {
+  console.log(`Area: ${a * a}`);
+  console.log(`Perimeter: ${a * 4}`);
 }
 
-table.appendChild(headingRow);
-
-employees.forEach((item) => {
-  const row = document.createElement("tr");
-
-  for (let value of Object.values(item)) {
-    const col = document.createElement("td");
-    col.textContent = value;
-    row.appendChild(col);
-  }
-
-  table.appendChild(row);
-});
-
-const nameInput = document.querySelector("#name");
-const ageInput = document.querySelector("#age");
-const salaryInput = document.querySelector("#salary");
-const btn = document.querySelector("button");
-
-btn.addEventListener("click", function () {
-  const tr = document.createElement("tr");
-  const values = [nameInput.value, ageInput.value, salaryInput.value];
-
-  for (const value of values) {
-    const td = document.createElement("td");
-    td.textContent = value;
-    tr.appendChild(td);
-  }
-
-  table.appendChild(tr);
-});
+calcAreaAndPerimeter(2);
